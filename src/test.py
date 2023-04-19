e5b84862f55da1fefbfcc841d8d3d4af0d88f674
Pat Deegan <pat.devel@psychogenic.com>
2023-04-19 16:41:10 -0400

    Test framework 4

--- src/test.py
+++ src/test.py
@@ -21,6 +21,7 @@ displayProx = {
             'lowclose':     0b00101010,
             'exact':        0b00000001,
             'hiclose':      0b01000110,
+            'hifar':        0b11000100
 
 }
 
@@ -28,10 +29,10 @@ os.environ['COCOTB_RESOLVE_X'] = 'RANDOM'
 async def reset(dut):
     dut._log.info("reset")
     dut.rst.value = 1
-    await ClockCycles(dut.clk, 3)
+    await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
     dut.clk_config.value = 0 # 1khz clock
-    await ClockCycles(dut.clk, 3)
+    await ClockCycles(dut.clk, 15)
     
     
 async def startup(dut):
@@ -43,21 +44,22 @@ async def getDisplayValues(dut):
     displayedValues = [None, None]
     attemptCount = 0
     while None in displayedValues or attemptCount < 3:
-        await ClockCycles(dut.clk, 1)
         displayedValues[int(dut.prox_select.value)] = int(dut.segments.value & 0x7f)
+        
+        await ClockCycles(dut.clk, 1)
         
         attemptCount += 1
         if attemptCount > 100:
             dut._log.error(f"NEVER HAVE {displayedValues}")
             return displayedValues
             
-    dut._log.info(f'DISPVALS: {displayedValues}')
+    dut._log.info(f'Display Segments: {displayedValues} ( [ {bin(displayedValues[0])} , {bin(displayedValues[1])}])')
     return displayedValues
     
 async def inputPulsesFor(dut, tunerInputFreqHz:int, inputTimeSecs=0.51, sysClkHz=1e3):
     numPulses = tunerInputFreqHz * inputTimeSecs 
     pulsePeriod = 1/tunerInputFreqHz
-    pulseHalfCycleUs = math.ceil(1e6*pulsePeriod/2.0)
+    pulseHalfCycleUs = round(1e6*pulsePeriod/2.0)
     
     displayedValues = [None, None]
         
@@ -81,13 +83,12 @@ async def setup_tuner(dut):
 async def note_toggle(dut, freq, delta=0, msg=""):
     dut._log.info(msg)
     await startup(dut)
-    dispValues = await inputPulsesFor(dut, freq + delta, 1.6)  
+    dispValues = await inputPulsesFor(dut, freq + delta, 1.65)  
     return dispValues
     
     
 
 async def note_e(dut, eFreq=330, delta=0, msg=""):
-    freq = eFreq
     
     dispValues = await note_toggle(dut, freq=eFreq, delta=delta, msg=msg);
     assert dispValues[1] == (displayNotes['E'] & 0x7F)
@@ -113,9 +114,15 @@ async def note_b(dut, delta=0, msg=""):
     
 @cocotb.test()
 async def note_e_lowclose(dut):
-    dispValues = await note_e(dut, eFreq=83, delta=-7, msg="E low/close")
+    dispValues = await note_e(dut, eFreq=83, delta=-6, msg="E low/close")
     assert dispValues[0] == (displayProx['lowclose'] & 0x7F) 
 
+
+    
+@cocotb.test()
+async def note_e_highfar(dut):
+    dispValues = await note_e(dut, eFreq=330, delta=9, msg="E high/far")
+    assert dispValues[0] == (displayProx['hifar'] & 0x7F) 
 
 @cocotb.test()
 async def note_g_lowclose(dut):
